c40a50555679d44224b5b96be8428be0c11bcbb2
aavidsoft-developer <aavidsoft.developer@gmail.com>
2023-08-14 14:22:21 +0530

    Anonymous classes - II

--- src/Circle.java
+++ src/Circle.java
@@ -3,4 +3,9 @@ public class Circle extends Shape{
     public void draw() {
         System.out.println("Circle");
     }
+
+    public float area() {
+        //some logic goes here
+        return 0;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         //way 1
         Circle c1 = new Circle();
         c1.draw();
+        c1.area();
 
         //way 2
         class CircleNew extends Shape {
@@ -11,10 +12,19 @@ public class Main {
             public void draw() {
                 System.out.println("CircleNew");
             }
+
+            public float area() {
+                return 0;
+            }
         }
 
         CircleNew cn1 = new CircleNew();
         cn1.draw();
+        cn1.area();
+
+        Shape s1 = new CircleNew();
+        s1.draw();
+        ((CircleNew)s1).area(); //down-casting , be careful
 
         //way 3
         Shape s = new Shape() {
@@ -22,8 +32,15 @@ public class Main {
             public void draw() {
                 System.out.println("New Shape");
             }
+
+            public float area() {
+                return 0;
+            }
         };
+
         s.draw();
+        //s.area();
+        //( (DerivedTypeName)s).area();
 
         //way 1
         Animal a = new Animal();
@@ -35,9 +52,14 @@ public class Main {
             public void move(int x, int y) {
                 System.out.println("AnimalNew Move: " + x + " " + y);
             }
+
+            public void info() {
+
+            }
         }
         AnimalNew animalNew = new AnimalNew();
         animalNew.move(200, 200);
+        animalNew.info();
 
         Movable m = new Movable() {
             @Override
